0ded6620ba4698baefa8fd4201e882db131d8fdb
hyejin <hyejda00@naver.com>
2024-06-10 20:20:02 +0900

    2024.06.10 백준 브론즈5 문제해결 완

---
+++ src/algotiyhmTest_20240610/baekjun11021.java
@@ -0,0 +1,33 @@
+// 백준 11021번 A+B - 7, 난이도 : 브론즈 5
+// 메모리 14616KB, 시간 124ms
+// 1. 테스트 케이스 갯수 N 입력받기
+// 2. 두 수 입력 받아서 stringTokenizer로 자르기
+// 3. for문 사용해서 두수 합쳐서 출력
+package algotiyhmTest_20240610;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjun11021 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append("Case #"+i+": ");
+            sb.append(Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken()));
+
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+
+
+
+    }
+}
---
+++ src/algotiyhmTest_20240610/baekjun11022.java
@@ -0,0 +1,31 @@
+// 백준 11022번 A+B - 8, 난이도 : 브론즈 5
+// 메모리 14176KB, 시간 : 128ms
+// 1. 테스트케이스 갯수 N 입력받기
+// 2. StringTokenizer로 공백 자른후 가져오기
+
+package algotiyhmTest_20240610;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class baekjun11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // 1. 테스트케이스 갯수 N 입력받기
+        int N = Integer.parseInt(br.readLine());
+
+        String[] num;
+        int[] intNum = new int[2];
+        for( int i = 1 ; i <= N ; i++){
+            num = br.readLine().split(" ");
+            intNum[0] = Integer.parseInt(num[0]);
+            intNum[1] = Integer.parseInt(num[1]);
+//            sb.append("Case #" +i + ": " + intNum[0] + " + " + intNum[1] + " = " + (intNum[0]+intNum[1]) + "\n");
+            sb.append("Case #").append(i).append(": ").append(intNum[0]).append(" + ").append(intNum[1]).append(" = ").append(intNum[0]+intNum[1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
