f65c9b1c61ef11abf52719dcab552ee70ba15bc2
Jeon Hyejin <hyejda00@naver.com>
2024-08-30 23:34:24 +0900

    2024.08.30 백준 브론즈5 문제해결 완

---
+++ src/algorithmTest_20240830/baekjun2743.java
@@ -0,0 +1,19 @@
+// 백준 2743번, 난이도 : 브론즈 5
+// 메모리 : 14248KB, 시간 : 100ms
+
+
+package algorithmTest_20240830;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjun2743 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str.length());
+    }
+}
