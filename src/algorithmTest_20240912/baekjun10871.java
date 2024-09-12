5c49c967fe1589a57676ef97576b4912110f0250
전혜진 <hyejda00@naver.com>
2024-09-12 23:59:09 +0900

    2024.09.12 백준 문제해결 완

---
+++ src/algorithmTest_20240912/baekjun10871.java
@@ -0,0 +1,34 @@
+// 백준 10871번 X보다 작은 수
+
+package algorithmTest_20240912;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baekjun10871 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < X) {
+                bw.write(arr[i] + " ");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
